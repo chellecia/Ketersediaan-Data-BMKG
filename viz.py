7a77549bff6186eaa3ad0aa7eaffa32665989d4f
Michelle V <147906882+chellecia@users.noreply.github.com>
2025-09-10 08:41:13 +0700

    Update viz.py

--- viz.py
+++ viz.py
@@ -81,26 +81,12 @@ def show_metar_visualizations(df_metar: pd.DataFrame, return_figs=True):
         names="Status",
         values="Jumlah",
         title="Distribusi Status Ketersediaan",
-        
-       color_discrete_sequence=px.colors.qualitative.Safe
+        color_discrete_sequence=px.colors.qualitative.Safe
     )
-
-    # Atur judul supaya tidak ketimpa
-    fig2.update_layout(
-    title=dict(
-        text="Distribusi Status Ketersediaan",
-        y=1.02,   # geser ke atas
-        x=0.0,    # posisi horizontal (tengah)
-        xanchor ="left",
-        pad=dict(l=10, r=0, t=20, b=0)
-    ),
-    margin=dict(t=150, l=100, r=80, b=80)
-
 
     fig2.update_traces(textinfo='percent+label')
     st.plotly_chart(fig2, use_container_width=True)
     figs.append(("pie_status.png", fig2))
-
 
     if return_figs:
         fixed_figs = [(fname, fix_figure_colors(fig)) for fname, fig in figs]
@@ -279,6 +265,7 @@ def show_speci_visualizations(df_speci_harian: pd.DataFrame, df_speci_bulanan: p
     if return_figs:
         fixed_figs = [(fname, fix_figure_colors(fig)) for fname, fig in figs]
         return fixed_figs
+
 
 
 

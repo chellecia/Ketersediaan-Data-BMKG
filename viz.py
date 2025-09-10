2b989f58519795fc3ba022eecbd301081f11128a
Michelle V <147906882+chellecia@users.noreply.github.com>
2025-09-10 08:37:47 +0700

    Update viz.py

--- viz.py
+++ viz.py
@@ -89,13 +89,13 @@ def show_metar_visualizations(df_metar: pd.DataFrame, return_figs=True):
     fig2.update_layout(
     title=dict(
         text="Distribusi Status Ketersediaan",
-        y=0.95,   # geser ke atas
+        y=1.02,   # geser ke atas
         x=0.0,    # posisi horizontal (tengah)
         xanchor ="left",
-        yanchor="top",
+        yanchor="bottom",
         pad=dict(l=10, r=0, t=20, b=0)
     ),
-    margin=dict(t=100)
+    margin=dict(t=150, l=100, r=80, b=80)
 )
 
     fig2.update_traces(textinfo='percent+label')
@@ -280,6 +280,7 @@ def show_speci_visualizations(df_speci_harian: pd.DataFrame, df_speci_bulanan: p
     if return_figs:
         fixed_figs = [(fname, fix_figure_colors(fig)) for fname, fig in figs]
         return fixed_figs
+
 
 
 
